69db97f64d1e2d5ca46563770818ef7323a5a9ec
Samuel Elkind <samuel.elkind@gmail.com>
2022-03-06 11:23:55 -0700

    fix prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import * as d3 from "d3";
-import './style.css'
+import * as d3 from 'd3';
+import './style.css';
 
 const component = () => {
     const element = document.createElement('div');
     element.innerHTML = 'Hello World';
-    element.classList.add('hello')
-    return element
-}
-document.body.appendChild(component())
+    element.classList.add('hello');
+    return element;
+};
+document.body.appendChild(component());
 
-d3.select("p").style("color", "blue")
+d3.select('p').style('color', 'blue');
