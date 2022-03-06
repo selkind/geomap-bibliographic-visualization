8ee0e4770e711a78eec50397f5c264ed6b132d65
Samuel Elkind <samuel.elkind@gmail.com>
2022-03-06 11:18:08 -0700

    project template complete

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,8 @@ const component = () => {
     const element = document.createElement('div');
     element.innerHTML = 'Hello World';
     element.classList.add('hello')
-
-
     return element
 }
-
 document.body.appendChild(component())
-d3.select("div").style("color", "blue")
+
+d3.select("p").style("color", "blue")
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 const path = require("path");
+const ForkTsCheckerNotifierWebpackPlugin = require('fork-ts-checker-notifier-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    context: process.cwd(),
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/index.ts',
     devtool: 'inline-source-map',
     devServer: {
         static: './dist',
     },
     plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new ForkTsCheckerNotifierWebpackPlugin({
+            title: 'Typescript', excludeWarnings: false
+        }),
+
         new HtmlWebpackPlugin({
+            inject: true,
             title: 'Output Management',
             template: "./src/index.html"
         })
     ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
@@ -22,11 +34,21 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.tsx?$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                use: [
+                    {loader: 'ts-loader', options: { transpileOnly: true}}
+                ],
+            },
+            {
                 test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'),
                 use: ['style-loader', 'css-loader']
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                include: path.resolve(__dirname, 'src'),
                 type: 'asset/resource'
             }
         ]
